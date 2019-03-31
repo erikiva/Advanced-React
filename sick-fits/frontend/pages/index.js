a43c4f738a24c516cee389c936874dcec05c97f1
natalia <nataliav@gmail.com>
2019-03-31 10:06:22 +0200

    Initial frontend setup

---
+++ sick-fits/frontend/pages/index.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import Link from "next/link";
+
+class Home extends Component {
+  render() {
+    return <div>Hey!</div>;
+  }
+}
+
+export default Home;
